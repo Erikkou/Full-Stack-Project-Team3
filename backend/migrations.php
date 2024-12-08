53931e16d61e8bba837b263f40c7363574f3fd91
NverKarapetyanNL <nver.am@live.nl>
2024-12-08 20:13:56 +0100

    feature/user-account: implementatie van user account op backend

---
+++ backend/migrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'migrations_paths' => [
+        // Namespace en directory waar je migraties worden opgeslagen
+        'App\Migrations' => './migrations',
+    ],
+    'all_or_nothing' => true,
+    'check_database_platform' => true,
+];
---
+++ backend/src/Entity/User.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+
+    public const ROLE_USER = 'ROLE_USER';
+    public const ROLE_ADMIN = 'ROLE_ADMIN';
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $email = null;
+
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
+
+//    #[ORM\OneToMany(targetEntity: Team::class, mappedBy: 'user')]
+//    private $teams;
+
+    public function __construct()
+    {
+        $this->teams = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(UserPasswordHasherInterface $passwordHasher, string $plainPassword): static
+    {
+        $this->password = $passwordHasher->hashPassword($this, $plainPassword);
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        // Voeg altijd de standaardrol toe
+        if (!in_array(self::ROLE_USER, $roles)) {
+            $roles[] = self::ROLE_USER;
+        }
+
+        return array_unique($roles);
+    }
+
+    private static array $validRoles = [
+        self::ROLE_USER,
+        self::ROLE_ADMIN,
+    ];
+
+    public function setRoles(array $roles): static
+    {
+        foreach ($roles as $role) {
+            if (!in_array($role, self::$validRoles)) {
+                throw new \InvalidArgumentException(sprintf('Invalid role "%s"', $role));
+            }
+        }
+
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function addRole(string $role): static
+    {
+        if (!in_array($role, self::$validRoles)) {
+            throw new \InvalidArgumentException(sprintf('Invalid role "%s"', $role));
+        }
+
+        if (!in_array($role, $this->roles)) {
+            $this->roles[] = $role;
+        }
+
+        return $this;
+    }
+
+
+    public function eraseCredentials(): void
+    {
+        // Implement this method if needed
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->username;
+    }
+}
