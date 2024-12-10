90a335132ca20c9c45de1f5e456eef3dc2ce9d3a
Antonio <a.mazzara@st.hanze.nl>
2024-12-10 20:45:32 +0100

    navigatiebalk toegevoegd

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">Over Ons</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
