71eee1e33dbb23dcc119e9f50ab64bbc4c0c4df9
Antonio <a.mazzara@st.hanze.nl>
2024-12-19 23:50:09 +0100

    table added by the dashboard

--- frontend/src/components/AdminLayout.js
+++ frontend/src/components/AdminLayout.js
@@ -47,7 +47,80 @@ const AdminLayout = ({ children }) => {
         </div>
 
         {/* Children Content */}
-        <div className="bg-white p-6 rounded-lg shadow-md">{children}</div>
+        
+
+        {/* Table */}
+
+<div class="flex flex-col overflow-x-auto bg-red-800 opacity-90 ml-10 mr-10">
+  <div class="sm:-mx-6 lg:-mx-8">
+    <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+      <div class="overflow-x-auto">
+        <table
+          class="min-w-full text-start text-sm font-light text-surface dark:text-white">
+          <thead
+            class="border-b border-neutral-200 font-medium dark:border-white/10">
+            <tr>
+              <th scope="col" class="px-6 py-4">#</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+              <th scope="col" class="px-6 py-4">Heading</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b border-neutral-200 dark:border-white/10">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+            </tr>
+            <tr class="border-b border-neutral-200 dark:border-white/10">
+              <td class="whitespace-nowrap px-6 py-4 font-medium ">2</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4 ">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4 ">Cell</td>
+            </tr>
+            <tr class="border-b border-neutral-200 dark:border-white/10">
+              <td class="whitespace-nowrap px-6 py-4 font-medium ">3</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4 ">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4 ">Cell</td>
+            </tr>
+            <tr class="border-b ">
+              <td class="whitespace-nowrap px-6 py-4 font-medium ">4</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+              <td class="whitespace-nowrap px-6 py-4">Cell</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>{children}</div>
       </main>
     </div>
   );
